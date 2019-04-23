0f9037b2f8c45f35426b917ad7ff10dc9485824b
Trong Nguyen <trong.nguyen@c02xl3uujgh5.ad.deliveryhero.com>
2019-04-23 13:27:00 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require ('body-parser');
+const graphQlHttp = require ('express-graphql');
+const { buildSchema } = require('graphql')
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/graphql', graphQlHttp({
+schema: buildSchema( `
+type RootQuery {
+events: [String!]!
+
+}
+type RootMutation {
+createEvent(name: String): String
+}
+
+schema {
+query: RootQuery
+mutation: RootMutation
+}
+`
+),
+rootValue: {
+events: () => {
+return ['Romantic Cookng', 'Sailing', 'All-Night Coding'];
+},
+
+createEvent: (args) => {
+const eventName = args.name;
+return eventName;
+}
+},
+graphiql: true
+
+})
+);
+
+app.listen(3000);
