b7e436c95d69346aa5e2d4cc291f6ae684395468
Trong Nguyen <trong.nguyen@c02xl3uujgh5.ad.deliveryhero.com>
2019-05-03 18:10:18 +0200

    hitting API

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
+import BookingsPage from './pages/Bookings';
+import EventsPage from './pages/Events';
+import MainNavigation from './components/Navigation/MainNavigation'
+
+
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter> 
+    <React.Fragment>
+    <MainNavigation/>
+    <main className="main-content">
+    <Switch>
+    <Redirect from="/" to="/auth" exact/>
+    <Route path="/auth" component={AuthPage} />
+    <Route path="/events" component={EventsPage} />
+    <Route path="/bookings" component={BookingsPage} />
+    <Route path="/" component={null} />
+    </Switch>
+    </main>
+    </React.Fragment>
+    </BrowserRouter> 
+  );
+}
+
+export default App;
